a350388654898d9834fb5f4d56d8d1d0d56608ce
Wesam Abadi <wisamabady7@gmail.com>
2023-12-06 10:46:53 +0100

    try catch

--- app.js
+++ app.js
@@ -2,73 +2,78 @@ import puppeteer from "puppeteer";
 import cheerio from "cheerio";
 import express from "express";
 
-    console.log("Job done");
+console.log("Job done");
 
 const app = express();
-    console.log("Job done");
+console.log("Job done");
 
 const PORT = process.env.PORT || 8000;
-    console.log("Job done");
-
+console.log("Job done");
 
 async function startRephrase(userInput) {
-        console.log("Job done");
-
-  const browser = await puppeteer.launch({
-    headless: true,
-  });
+  try {
     console.log("Job done");
 
-  const page = await browser.newPage();
-      console.log("Job done");
-
-  await page.setUserAgent(
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
-  );
-      console.log("Job done");
-
-  await page.setViewport({ width: 1280, height: 1024 });
-      console.log("Job done");
-
-  await page.goto("https://typeset.io/paraphraser");
-      console.log("Job done");
-
-  const textToMatch = "Write here or ";
-      console.log("Job done");
-
-  const xpathSelector = `//p[contains(text(), "${textToMatch}")]`;
-  await page.waitForXPath(xpathSelector);
-  const [inputForm] = await page.$x(xpathSelector);
-  const [rephrasButton] = await page.$x("//button[contains(., 'Paraphrase')]");
-
-  if (inputForm) {
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
     console.log("Job done");
 
-    await inputForm.click();
-    await page.evaluate(
-      () => new Promise((resolve) => setTimeout(resolve, 500))
+    const page = await browser.newPage();
+    console.log("Job done");
+
+    await page.setUserAgent(
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
     );
-    await page.keyboard.type(userInput);
     console.log("Job done");
 
-    await rephrasButton.click();
+    await page.setViewport({ width: 1280, height: 1024 });
     console.log("Job done");
 
-    await page.waitForSelector('svg[data-icon="copy"]');
-    const htmlContent = await page.$eval(
-      '[data-paraphraser-output="true"]',
-      (div) => div.outerHTML
+    await page.goto("https://typeset.io/paraphraser");
+    console.log("Job done");
+
+    const textToMatch = "Write here or ";
+    console.log("Job done");
+
+    const xpathSelector = `//p[contains(text(), "${textToMatch}")]`;
+    await page.waitForXPath(xpathSelector);
+    const [inputForm] = await page.$x(xpathSelector);
+    const [rephrasButton] = await page.$x(
+      "//button[contains(., 'Paraphrase')]"
     );
-    const $ = cheerio.load(htmlContent);
-    console.log("Job done");
 
-    const textContent = $('div[data-paraphraser-output="true"]').text();
-    await browser.close();
-    console.log("Job done");
-    return { textContent };
-  } else {
-    await browser.close();
-    return { error: "Could not find input form." };
+    if (inputForm) {
+      console.log("Job done");
+
+      await inputForm.click();
+      await page.evaluate(
+        () => new Promise((resolve) => setTimeout(resolve, 500))
+      );
+      await page.keyboard.type(userInput);
+      console.log("Job done");
+
+      await rephrasButton.click();
+      console.log("Job done");
+
+      await page.waitForSelector('svg[data-icon="copy"]');
+      const htmlContent = await page.$eval(
+        '[data-paraphraser-output="true"]',
+        (div) => div.outerHTML
+      );
+      const $ = cheerio.load(htmlContent);
+      console.log("Job done");
+
+      const textContent = $('div[data-paraphraser-output="true"]').text();
+      await browser.close();
+      console.log("Job done");
+      return { textContent };
+    } else {
+      await browser.close();
+      return { error: "Could not find input form." };
+    }
+  } catch (error) {
+    console.error("Error in startRephrase:", error);
   }
 }
 
