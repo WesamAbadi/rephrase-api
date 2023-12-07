99dfdce7380abf09305fd1226780d5ec9860a932
Wesam Abadi <wisamabady7@gmail.com>
2023-12-07 23:47:53 +0100

    headless new

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let browserInstance;
 async function getBrowserInstance() {
   if (!browserInstance) {
     browserInstance = await puppeteer.launch({
-      headless: true,
+      headless: new,
     });
   }
   return browserInstance;
